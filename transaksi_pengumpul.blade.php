52babc45a84ae93e394f9e41ec3d6252cc8bfc1b
Nisa-Fakhira <63064954+Nisa-Fakhira@users.noreply.github.com>
2020-05-15 08:50:13 +0700

    add transaksi_pengumpul
    add transaksi_pengumpul.blade.php

---
+++ transaksi_pengumpul.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Saldo</title>
+    <style>
+    .tr {
+        text-align: center;
+    }
+    </style>
+</head>
+<body>
+    <h1 class="tr">SALDO</h1>
+    <form method="post">
+        Saldo Anda Saat Ini: <br><br> <input type="text" name="tujuan" />
+    </form>
+    <p> <input type="submit" name="submit" value="Transfer" /> 
+        </form>
+    </p>
+</body>
+</html>
