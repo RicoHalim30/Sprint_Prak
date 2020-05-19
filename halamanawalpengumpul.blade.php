d99e23a65bc6024458a35db7accd58dc02bc0c8f
Nisa-Fakhira <63064954+Nisa-Fakhira@users.noreply.github.com>
2020-05-19 23:09:56 +0700

    Adding halaman awal pengumpul
    halamanawalpengumpul.blade.php

---
+++ halamanawalpengumpul.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+    <style>
+    .tr {
+        text-align: center;
+    }
+    </style>
+    <header class="header">
+		<ul>
+			<a href="#">Menu</a>
+        </ul>
+</head>
+<form method="post">
+    <h1 class="tr">SELAMAT DATANG</h1><br>
+    <h3 class="tr"><input type="text" name="tujuan" /></h3><br>
+    <h3 class="tr"> Saldo Anda <br> <input type="text" name="berat" /></h3><br>
+    <h3 class="tr"> Total Sampah Yang Dikumpulkan <br> <input type="text" name="berat" /></h3><br><br>
+</form>
+<html>
